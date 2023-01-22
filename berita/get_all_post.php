277fe3fc33ee695d1c7493f310cf9798135d9a7f
Ryan Bagus <ryanbagusha@gmail.com>
2023-01-22 18:20:23 +0800

    api posts

--- berita/get_all_post.php
+++ berita/get_all_post.php
@@ -5,26 +5,23 @@ require_once("../conn.php");
 mysqli_connect_errno();
 date_default_timezone_set('Asia/Jakarta');
 
-$json = array(
-    "response_status" => "OK",
-    "response_message" => '',
-    "data" => array()
-);
+$json = array();
 
 $sql = $conn->query("SELECT * FROM berita WHERE id_type = 1");
 $jml = $sql->num_rows;
 if ($jml > 0) {
     while ($data = $sql->fetch_object()) {
         $arr_row = array();
+        $arr_row['id'] = $data->id_berita;
         $arr_row['judul'] = $data->judul;
+        $arr_row['tanggal'] = $data->tanggal;
         // $arr_row['isi'] = $data->isi;
         $arr_row['media'] = $data->media;
-        $arr_row['total_view'] = $data->total_view;
-        $json['data'][] = $arr_row;
+        // $arr_row['total_view'] = $data->total_view;
+        $json[] = $arr_row;
     }
 } else {
-    $json['response_status'] = "Error";
-    $json['response_message'] = "Data Kosong";
+    $json[] = "Error";
 }
 
 header('Content-Type: application/json');
